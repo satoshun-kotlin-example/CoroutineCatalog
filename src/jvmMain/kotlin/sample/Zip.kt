81374f773609665c8346b4ae77dcacf5968c93e0
SatoShun <shun.sato1@gmail.com>
2019-03-10 07:41:58 +0000

    too simply zip operations

--- src/jvmMain/kotlin/sample/Zip.kt
+++ src/jvmMain/kotlin/sample/Zip.kt
@@ -1,4 +1,11 @@
 package sample
 
-fun zip() {
+import kotlinx.coroutines.Deferred
+
+suspend inline fun <T1, T2> zip(t1: Deferred<T1>, t2: Deferred<T2>): Pair<T1, T2> {
+  return t1.await() to t2.await()
+}
+
+suspend inline fun <T1, T2> Deferred<T1>.zipWith(t2: Deferred<T2>): Pair<T1, T2> {
+  return await() to t2.await()
 }
