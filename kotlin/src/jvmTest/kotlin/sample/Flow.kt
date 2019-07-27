f7fa2e8b4150da02e455bed4ac9ae03a9e28bd03
SatoShun <shun.sato1@gmail.com>
2019-07-27 10:33:50 +0000

    update

---
+++ kotlin/src/jvmTest/kotlin/sample/Flow.kt
@@ -0,0 +1,30 @@
+package sample
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+
+class FlowTest {
+  @Test
+  fun test() = runBlockingTest {
+    val f = flow {
+      delay(100)
+      emit(100)
+    }
+
+    f
+      .map { it * 2 }
+      .collect {
+        println(it)
+      }
+
+    f
+      .map { it * 2 }
+      .collect {
+        println(it)
+      }
+  }
+}
