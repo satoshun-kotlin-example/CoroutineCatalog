8b4d985edbb6106011d97814aece71939aedcd6c
SatoShun <shun.sato1@gmail.com>
2019-08-02 00:51:01 +0000

    flow

--- android/app/src/main/java/com/github/satoshun/example/coroutine/MainActivity.kt
+++ android/app/src/main/java/com/github/satoshun/example/coroutine/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.github.satoshun.example.coroutine
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
   private lateinit var viewModel: MainViewModel
@@ -50,6 +56,39 @@ class MainActivity : AppCompatActivity() {
     val button6: Button = findViewById(R.id.hello6)
     button6.setOnClickListener {
       viewModel.parallelTaskWithCancellation()
+    }
+
+    val f = flow {
+      val currentTime = System.currentTimeMillis()
+      repeat(3) {
+        delay(1000)
+        emit("$it ${System.currentTimeMillis() - currentTime} ${Thread.currentThread()}")
+      }
+    }
+
+    // Launch Thread: Main
+    // consumer: rapid
+    // flowOn:
+    lifecycleScope.launch {
+      Log.d("t1", "start ${Thread.currentThread()}")
+      f
+        .collect {
+          Log.d("t1", "$it : ${Thread.currentThread()}")
+        }
+      Log.d("t1", "start ${Thread.currentThread()}")
+    }
+
+    // Launch Thread: Main
+    // consumer: slow
+    // flowOn:
+    lifecycleScope.launch {
+      Log.d("t2", "start ${Thread.currentThread()}")
+      f
+        .collect {
+          delay(2500)
+          Log.d("t2", "$it : ${Thread.currentThread()}")
+        }
+      Log.d("t2", "finish ${Thread.currentThread()}")
     }
   }
 }
