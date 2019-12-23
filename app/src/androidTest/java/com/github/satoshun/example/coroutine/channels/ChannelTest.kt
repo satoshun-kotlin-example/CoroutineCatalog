7a510460fb186dcffd90262c7925256e4614f97c
SatoShun <shun.sato1@gmail.com>
2019-12-23 03:31:54 +0000

    add BroadcastChannel

--- app/src/androidTest/java/com/github/satoshun/example/coroutine/channels/ChannelTest.kt
+++ app/src/androidTest/java/com/github/satoshun/example/coroutine/channels/ChannelTest.kt
@@ -4,7 +4,9 @@ import com.github.satoshun.example.coroutine.MainCoroutineRule
 import com.github.satoshun.example.coroutine.runBlocking
 import com.github.satoshun.example.coroutine.scope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
@@ -15,7 +17,7 @@ import org.junit.Test
 class ChannelTest {
   @get:Rule val coroutineRule = MainCoroutineRule()
 
-  // - conflated
+  // conflated
   @Test
   fun test1() = coroutineRule.runBlocking {
     val channel = Channel<String>(Channel.CONFLATED)
@@ -28,6 +30,29 @@ class ChannelTest {
 
     coroutineRule.scope().launch {
       channel.consumeAsFlow().collect {
+        println("2 $it")
+      }
+    }
+
+    channel.send("test1")
+    channel.send("test2")
+    channel.send("test3")
+    channel.send("test4")
+  }
+
+  // conflated
+  @Test
+  fun test2() = coroutineRule.runBlocking {
+    val channel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    coroutineRule.scope().launch {
+      channel.asFlow().collect {
+        println("1 $it")
+      }
+    }
+
+    coroutineRule.scope().launch {
+      channel.asFlow().collect {
         println("2 $it")
       }
     }
