3d752600591afe60c13116ef9957d7b54689ed18
SatoShun <shun.sato1@gmail.com>
2020-01-20 12:22:29 +0000

    launchTest2

--- app/src/test/java/com/github/satoshun/example/coroutine/AsyncCoroutineScopeTest.kt
+++ app/src/test/java/com/github/satoshun/example/coroutine/AsyncCoroutineScopeTest.kt
@@ -5,22 +5,30 @@ import com.github.satoshun.example.coroutine.rule.runBlocking
 import com.github.satoshun.example.coroutine.rule.scope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import java.io.IOException
+import kotlin.coroutines.resumeWithException
 
 @ExperimentalCoroutinesApi
 class AsyncCoroutineScopeTest {
   @get:Rule val coroutineRule = MainCoroutineRule()
 
+  @Ignore("throw exception")
   @Test
   fun launchTest() = coroutineRule.runBlocking {
     println("start")
-    coroutineScope {
-      coroutineRule.scope().launchException()
-    }
+    runCatching { coroutineRule.scope().launchException() }
+    println("end")
+  }
+
+  @Test
+  fun launchTest2() = coroutineRule.runBlocking {
+    println("start")
+    runCatching { launchException2() }
     println("end")
   }
 }
@@ -29,4 +37,9 @@ fun CoroutineScope.launchException() {
   launch {
     throw IOException("launchException")
   }
+}
+
+suspend fun launchException2() = suspendCancellableCoroutine<Unit> { cont ->
+  cont.resumeWithException(IOException("launchException"))
+//  throw IOException("launchException")
 }
