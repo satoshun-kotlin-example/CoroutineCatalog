904d15467c215ab22f70673745ecd47500910712
SatoShun <shun.sato1@gmail.com>
2020-05-09 01:36:42 +0000

    state flow

--- app/src/test/java/com/github/satoshun/example/coroutine/flow/StateFlowTest.kt
+++ app/src/test/java/com/github/satoshun/example/coroutine/flow/StateFlowTest.kt
@@ -1,13 +1,35 @@
 package com.github.satoshun.example.coroutine.flow
 
 import com.github.satoshun.example.coroutine.rule.MainCoroutineRule
+import com.github.satoshun.example.coroutine.rule.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class StateFlowTest {
   @get:Rule val coroutineRule = MainCoroutineRule()
 
-  @Test
-  fun noInitialValue() {
+  private lateinit var viewModel: ViewModelTest
+
+  @Before
+  fun setUp() {
+    viewModel = ViewModelTest()
   }
+
+  @Test
+  fun nullInitialValue() {
+    coroutineRule.runBlocking {
+      viewModel
+        .nullable
+        .first()
+    }
+  }
+}
+
+private class ViewModelTest {
+  val nullable = MutableStateFlow<Int?>(null)
 }
