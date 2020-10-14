47aedcd3c5d17b6fdf851270acaac8f3ba11fe11
SatoShun <shun.sato1@gmail.com>
2020-10-14 02:12:00 +0000

    add stateflow sample

--- app/src/main/java/com/github/satoshun/example/coroutine/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/coroutine/MainActivity.kt
@@ -1,243 +1,256 @@
 package com.github.satoshun.example.coroutine
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.callbackFlow
+import androidx.lifecycle.observe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
   private lateinit var viewModel: MainViewModel
 
+  @OptIn(ExperimentalCoroutinesApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get()
-
-    viewModel.old
-      .onEach {
-        println("HOGEFUGA: $it")
-      }
-      .launchIn(lifecycleScope)
-    lifecycleScope.launch {
-      delay(1000)
-      viewModel.source.send("test2")
-      delay(1)
-      viewModel.source.send("test")
-      delay(1000)
-      viewModel.source.send("test3")
-    }
-
-    viewModel.userName.observe(this, Observer {
-      Toast
-        .makeText(this@MainActivity, it!!, Toast.LENGTH_LONG)
-        .show()
-    })
-
-    val button: Button = findViewById(R.id.hello)
-    button.setOnClickListener {
-      viewModel.serialTask()
-    }
-
-    val button2: Button = findViewById(R.id.hello2)
-    button2.setOnClickListener {
-      viewModel.serialTaskWithError()
-    }
-
-    val button3: Button = findViewById(R.id.hello3)
-    button3.setOnClickListener {
-      viewModel.parallelTask()
-    }
-
-    val button4: Button = findViewById(R.id.hello4)
-    button4.setOnClickListener {
-      viewModel.parallelTaskWithError()
-    }
-
-    val button5: Button = findViewById(R.id.hello5)
-    button5.setOnClickListener {
-      viewModel.parallelTaskWithError2()
-    }
-
-    val button6: Button = findViewById(R.id.hello6)
-    button6.setOnClickListener {
-      viewModel.parallelTaskWithCancellation()
-    }
-
-    val f = flow {
-      val startTime = System.currentTimeMillis()
-      repeat(100) {
-        delay(1)
-        emit("$it ${System.currentTimeMillis() - startTime} ${Thread.currentThread()}")
-        println("$it emitted")
-      }
-    }
-
-    lifecycleScope.launch {
-      coroutineScope {
-        launch {
-          delay(10000)
-          println("hogehoge1")
-        }
-        launch {
-          delay(8000)
-          println("hogehoge2")
-        }
-      }
-      println("hogehoge3")
-    }
-
-    // Launch Thread: Main
-    // consumer: rapid
-    // flowOn:
+//    viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get()
+//
+//    viewModel.old
+//      .onEach {
+//        println("HOGEFUGA: $it")
+//      }
+//      .launchIn(lifecycleScope)
 //    lifecycleScope.launch {
-//      Log.d("t1", "start ${Thread.currentThread()}")
-//      f
-//        .collect {
-//          Log.d("t1", "$it : ${Thread.currentThread()}")
-//        }
-//      Log.d("t1", "start ${Thread.currentThread()}")
+//      delay(1000)
+//      viewModel.source.send("test2")
+//      delay(1)
+//      viewModel.source.send("test")
+//      delay(1000)
+//      viewModel.source.send("test3")
 //    }
 //
-//    // Launch Thread: Main
-//    // consumer: slow
-//    // flowOn:
+//    viewModel.userName.observe(this, Observer {
+//      Toast
+//        .makeText(this@MainActivity, it!!, Toast.LENGTH_LONG)
+//        .show()
+//    })
+//
+//    val button: Button = findViewById(R.id.hello)
+//    button.setOnClickListener {
+//      viewModel.serialTask()
+//    }
+//
+//    val button2: Button = findViewById(R.id.hello2)
+//    button2.setOnClickListener {
+//      viewModel.serialTaskWithError()
+//    }
+//
+//    val button3: Button = findViewById(R.id.hello3)
+//    button3.setOnClickListener {
+//      viewModel.parallelTask()
+//    }
+//
+//    val button4: Button = findViewById(R.id.hello4)
+//    button4.setOnClickListener {
+//      viewModel.parallelTaskWithError()
+//    }
+//
+//    val button5: Button = findViewById(R.id.hello5)
+//    button5.setOnClickListener {
+//      viewModel.parallelTaskWithError2()
+//    }
+//
+//    val button6: Button = findViewById(R.id.hello6)
+//    button6.setOnClickListener {
+//      viewModel.parallelTaskWithCancellation()
+//    }
+//
+//    val f = flow {
+//      val startTime = System.currentTimeMillis()
+//      repeat(100) {
+//        delay(1)
+//        emit("$it ${System.currentTimeMillis() - startTime} ${Thread.currentThread()}")
+//        println("$it emitted")
+//      }
+//    }
+//
 //    lifecycleScope.launch {
-//      Log.d("t2", "start ${Thread.currentThread()}")
-//      f
-//        .collect {
-//          delay(100)
-//          Log.d("t2", "$it : ${Thread.currentThread()}")
+//      coroutineScope {
+//        launch {
+//          delay(10000)
+//          println("hogehoge1")
 //        }
-//      Log.d("t2", "finish ${Thread.currentThread()}")
+//        launch {
+//          delay(8000)
+//          println("hogehoge2")
+//        }
+//      }
+//      println("hogehoge3")
 //    }
 //
 //    // Launch Thread: Main
 //    // consumer: rapid
+//    // flowOn:
+////    lifecycleScope.launch {
+////      Log.d("t1", "start ${Thread.currentThread()}")
+////      f
+////        .collect {
+////          Log.d("t1", "$it : ${Thread.currentThread()}")
+////        }
+////      Log.d("t1", "start ${Thread.currentThread()}")
+////    }
+////
+////    // Launch Thread: Main
+////    // consumer: slow
+////    // flowOn:
+////    lifecycleScope.launch {
+////      Log.d("t2", "start ${Thread.currentThread()}")
+////      f
+////        .collect {
+////          delay(100)
+////          Log.d("t2", "$it : ${Thread.currentThread()}")
+////        }
+////      Log.d("t2", "finish ${Thread.currentThread()}")
+////    }
+////
+////    // Launch Thread: Main
+////    // consumer: rapid
+////    // flowOn: Dispatchers.IO
+////    lifecycleScope.launch {
+////      Log.d("t3", "start ${Thread.currentThread()}")
+////      f
+////        .flowOn(Dispatchers.IO)
+////        .collect {
+////          Log.d("t3", "$it : ${Thread.currentThread()}")
+////        }
+////      Log.d("t3", "finish ${Thread.currentThread()}")
+////    }
+//
+//    // Launch Thread: Main
+//    // consumer: slow
 //    // flowOn: Dispatchers.IO
-//    lifecycleScope.launch {
-//      Log.d("t3", "start ${Thread.currentThread()}")
-//      f
-//        .flowOn(Dispatchers.IO)
-//        .collect {
-//          Log.d("t3", "$it : ${Thread.currentThread()}")
-//        }
-//      Log.d("t3", "finish ${Thread.currentThread()}")
+//    // buffer:
+////    lifecycleScope.launch {
+////      Log.d("t4", "start ${Thread.currentThread()}")
+////      val currentTime = System.currentTimeMillis()
+////      f
+////        .buffer(Channel.BUFFERED)
+////        .collect {
+////          delay(100)
+////          Log.d("t4", "$it : ${Thread.currentThread()} ${System.currentTimeMillis() - currentTime}")
+////        }
+////      Log.d("t4", "finish ${Thread.currentThread()}")
+////    }
+//
+//    val f2 = callbackFlow {
+//      val startTime = System.currentTimeMillis()
+//      repeat(100) {
+//        delay(10)
+//        send("$it ${System.currentTimeMillis() - startTime} ${Thread.currentThread()}")
+//        println("$it emitted")
+//      }
 //    }
-
-    // Launch Thread: Main
-    // consumer: slow
-    // flowOn: Dispatchers.IO
-    // buffer:
-//    lifecycleScope.launch {
-//      Log.d("t4", "start ${Thread.currentThread()}")
-//      val currentTime = System.currentTimeMillis()
-//      f
-//        .buffer(Channel.BUFFERED)
-//        .collect {
-//          delay(100)
-//          Log.d("t4", "$it : ${Thread.currentThread()} ${System.currentTimeMillis() - currentTime}")
-//        }
-//      Log.d("t4", "finish ${Thread.currentThread()}")
-//    }
-
-    val f2 = callbackFlow {
-      val startTime = System.currentTimeMillis()
-      repeat(100) {
-        delay(10)
-        send("$it ${System.currentTimeMillis() - startTime} ${Thread.currentThread()}")
-        println("$it emitted")
-      }
-    }
-
+//
+////    lifecycleScope.launch {
+////      Log.d("t4", "start ${Thread.currentThread()}")
+////      val currentTime = System.currentTimeMillis()
+////      f2
+////        .buffer(Channel.BUFFERED)
+////        .flowOn(Dispatchers.IO)
+////        .collect {
+////          delay(100)
+////          Log.d("t4", "$it : ${Thread.currentThread()} s${System.currentTimeMillis() - currentTime}")
+////        }
+////      Log.d("t4", "finish ${Thread.currentThread()}")
+////    }
+//
 //    lifecycleScope.launch {
 //      Log.d("t4", "start ${Thread.currentThread()}")
 //      val currentTime = System.currentTimeMillis()
 //      f2
-//        .buffer(Channel.BUFFERED)
+//        .conflate()
 //        .flowOn(Dispatchers.IO)
 //        .collect {
 //          delay(100)
-//          Log.d("t4", "$it : ${Thread.currentThread()} s${System.currentTimeMillis() - currentTime}")
+//          Log.d(
+//            "t4",
+//            "$it : ${Thread.currentThread()} s${System.currentTimeMillis() - currentTime}"
+//          )
 //        }
 //      Log.d("t4", "finish ${Thread.currentThread()}")
 //    }
+//
+//    supportFragmentManager
+//      .beginTransaction()
+//      .add(R.id.container, HogeFragment())
+//      .commit()
+//
+//    lifecycleScope.launchWhenResumed {
+//      println("hogehoge5")
+//    }
+//
+//    lifecycleScope.launch {
+//      val flow1 = flow {
+//        delay(100000)
+//        emit(100)
+//        emit(2000)
+//      }
+//      val flow2 = flow {
+//        delay(3000)
+//        emit(100)
+//        emit(2000)
+//      }
+//
+//      flow1.combine(flow2) { a, b ->
+//      }
+//
+//      coroutineScope {
+//        awaitAll(
+//          async {
+//            flow1.collect {
+//              print("HOGE1")
+//            }
+//          },
+//          async {
+//            flow2.collect {
+//              println("HOGE2")
+//            }
+//          }
+//        )
+//      }
+//      println("LAST")
+//    }
 
-    lifecycleScope.launch {
-      Log.d("t4", "start ${Thread.currentThread()}")
-      val currentTime = System.currentTimeMillis()
-      f2
-        .conflate()
-        .flowOn(Dispatchers.IO)
-        .collect {
-          delay(100)
-          Log.d(
-            "t4",
-            "$it : ${Thread.currentThread()} s${System.currentTimeMillis() - currentTime}"
-          )
+    fun liveDataAndStateFlowLifecycleTest() {
+      val liveData = MutableLiveData<Int>()
+      val stateFlow = MutableStateFlow(-1)
+      var current = 0
+
+      // simulate background processing
+      thread {
+        while (true) {
+          liveData.postValue(current)
+          stateFlow.value = current
+          current += 1
+          Thread.sleep(500)
         }
-      Log.d("t4", "finish ${Thread.currentThread()}")
+      }
+
+      lifecycleScope.launchWhenStarted {
+        stateFlow.collect {
+          println("stateFlow $it")
+        }
+      }
+      liveData.observe(owner = this) {
+        println("liveData $it")
+      }
     }
-
-    supportFragmentManager
-      .beginTransaction()
-      .add(R.id.container, HogeFragment())
-      .commit()
-
-    lifecycleScope.launchWhenResumed {
-      println("hogehoge5")
-    }
-
-    lifecycleScope.launch {
-      val flow1 = flow {
-        delay(100000)
-        emit(100)
-        emit(2000)
-      }
-      val flow2 = flow {
-        delay(3000)
-        emit(100)
-        emit(2000)
-      }
-
-      flow1.combine(flow2) { a, b ->
-      }
-
-      coroutineScope {
-        awaitAll(
-          async {
-            flow1.collect {
-              print("HOGE1")
-            }
-          },
-          async {
-            flow2.collect {
-              println("HOGE2")
-            }
-          }
-        )
-      }
-      println("LAST")
-    }
+    liveDataAndStateFlowLifecycleTest()
   }
 
   override fun onResume() {
