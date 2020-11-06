2e912801f6105dc45a7f93a815b7f5c80b2953eb
SatoShun <shun.sato1@gmail.com>
2020-11-06 04:44:11 +0000

    shared flow

---
+++ app/src/test/java/com/github/satoshun/example/coroutine/flow/SharedFlowTest.kt
@@ -0,0 +1,42 @@
+package com.github.satoshun.example.coroutine.flow
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import org.junit.Test
+
+class SharedFlowTest {
+    @Test
+    fun test(): Unit = runBlocking {
+        val flow = MutableSharedFlow<Int>()
+
+        val job = launch {
+            flow.emitWaitUntilCollector(100)
+
+            println("launch emitWaitUntilCollector")
+
+            flow.emitWaitUntilCollector(1200)
+        }
+
+
+        println("first ${job.isCancelled}")
+
+        println(flow.first())
+
+        println("second ${job.isCancelled}")
+
+        println(flow.first())
+
+        println("third")
+    }
+}
+
+suspend fun <T> MutableSharedFlow<T>.emitWaitUntilCollector(value: T) {
+    println("start coroutineScope")
+
+    subscriptionCount
+        .filter { it > 0 }
+        .onEach { emit(value) }
+        .first()
+
+    println("end coroutineScope")
+}
