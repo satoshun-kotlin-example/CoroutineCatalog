02d8ab08d006fe97c074fe2bc5e68e9faf86ecb3
SatoShun <shun.sato1@gmail.com>
2020-11-16 09:40:20 +0000

    shared flow

--- app/src/test/java/com/github/satoshun/example/coroutine/flow/SharedFlowTest.kt
+++ app/src/test/java/com/github/satoshun/example/coroutine/flow/SharedFlowTest.kt
@@ -5,38 +5,38 @@ import kotlinx.coroutines.flow.*
 import org.junit.Test
 
 class SharedFlowTest {
-    @Test
-    fun test(): Unit = runBlocking {
-        val flow = MutableSharedFlow<Int>()
+  @Test
+  fun test(): Unit = runBlocking {
+    val flow = MutableSharedFlow<Int>()
 
-        val job = launch {
-            flow.emitWaitUntilCollector(100)
+    val job = launch {
+      flow.emitWaitUntilCollector(100)
 
-            println("launch emitWaitUntilCollector")
+      println("launch emitWaitUntilCollector")
 
-            flow.emitWaitUntilCollector(1200)
-        }
-
-
-        println("first ${job.isCancelled}")
-
-        println(flow.first())
-
-        println("second ${job.isCancelled}")
-
-        println(flow.first())
-
-        println("third")
+      flow.emitWaitUntilCollector(1200)
     }
+
+
+    println("first ${job.isCancelled}")
+
+    println(flow.first())
+
+    println("second ${job.isCancelled}")
+
+    println(flow.first())
+
+    println("third")
+  }
 }
 
 suspend fun <T> MutableSharedFlow<T>.emitWaitUntilCollector(
-    value: T,
-    limitRefCount: Int = 1
+  value: T,
+  limitRefCount: Int = 1
 ) {
-    subscriptionCount
-        .filter { it >= limitRefCount }
-        .first()
+  subscriptionCount
+    .filter { it >= limitRefCount }
+    .first()
 
-    emit(value)
+  emit(value)
 }
